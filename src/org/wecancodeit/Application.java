7d8c1ddec7c4cedf668e86f1687d076de5ec38f4
oh-storebot <eric@negron.nu>
2018-08-26 14:09:24 -0400

    adding the family array

--- src/org/wecancodeit/Application.java
+++ src/org/wecancodeit/Application.java
@@ -5,10 +5,19 @@ import java.util.Scanner;
 public class Application {
 
 	public static void main(String[] args) {
+		int familyCount;
 		Scanner input = new Scanner(System.in);
 		System.out.println("What is the last name?");
 		String lastName = input.nextLine();
-		System.out.println("The last name is: " + lastName);
+		//System.out.println("The last name is: " + lastName);
+		System.out.println("What is the first name? ");
+		System.out.println("How many people in your family? ");
+		familyCount = input.nextInt();
+		FamilyMember[] familyMembers = new FamilyMember[familyCount];
+		
+		
+		//String firstName = input.nextLine();
+		//System.out.println(firstName);
 	}
 	
 	class FamilyMember {
